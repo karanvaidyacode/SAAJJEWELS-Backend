7006886b033b719a32e1ddbc4e08467efb1c1297
Devratna Sawaitul <devratnasawaitul6@gmail.com>
2025-12-07 15:15:48 +0530

    minor fixes

--- api/index.js
+++ api/index.js
@@ -1,35 +1,24 @@
-// api/index.js
-const serverless = require("serverless-http");
-const createApp = require("../src/server");
+const serverless = require('serverless-http');
+const createApp = require('../src/server');
 
 let initPromise = null;
 let handler = null;
 
 if (!initPromise) {
   initPromise = (async () => {
-    try {
-      const app = await createApp();
-      handler = serverless(app);
-      return handler;
-    } catch (err) {
-      console.error("Failed to init app for serverless:", err);
-      throw err;
-    }
+    const app = await createApp();
+    handler = serverless(app);
+    return handler;
   })();
 }
 
 module.exports = async (req, res) => {
   try {
     await initPromise;
-    if (!handler) {
-      res.statusCode = 500;
-      res.end("Server initialization failed");
-      return;
-    }
+    if (!handler) return res.status(500).end('Server init failed');
     return handler(req, res);
   } catch (err) {
-    console.error("Serverless handler error:", err);
-    res.statusCode = 500;
-    res.end("Internal Server Error");
+    console.error('SERVERLESS ERROR:', err);
+    res.status(500).end('Internal Server Error');
   }
 };
